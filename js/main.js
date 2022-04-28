df699db0349636f4de92f8658b70eb196331ff0b
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-04-28 15:45:35 +0200

    aggiunta variabile che calcola il prezzo base  del biglietto

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const Kilometers = Number(prompt ("Quanti chilometri è lungo il tragitto?"));
 //2.Chiedo l'età del passeggero
 const age = Number(prompt("Quanti anni ha il passeggero?"))
 //3.Calcolo il prezzo del biglietto
-
+let price = Kilometers * 0.21
+console.log(price);
 //4.SE il passeggero è minorenne applico uno sconto del 20%
 
 //5.SE il passeggerto è over 65 anno applico uno sconto del 60%
