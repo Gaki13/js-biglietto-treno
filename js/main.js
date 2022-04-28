14f61b615ded2eea0981a50669f2d3040ee28b2b
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-04-28 17:10:05 +0200

    correzzione dello sconto sugli over 65

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let price = Kilometers * 0.21
 price = price.toFixed(2);
 console.log(price);
 //4.SE il passeggero è minorenne applico uno sconto del 20%
-//5.SE il passeggerto è over 65 anno applico uno sconto del 60%
+//5.SE il passeggerto è over 65 anno applico uno sconto del 40%
 //6.SE l'età del passeggero è compresa tra 18 e 65 non applico nessuno sconto
 if (age < 18 ) {
     let discountChilds = price * 0.2
@@ -15,7 +15,7 @@ if (age < 18 ) {
     discountPrice = discountPrice.toFixed(2);
     document.getElementById("final-price").innerHTML = discountPrice + "€";
 } else if ( age >= 65 ){
-    let discountOlds = price * 0.6
+    let discountOlds = price * 0.4
     let discountPrice = price - discountOlds
     discountPrice = discountPrice.toFixed(2);
     document.getElementById("final-price").innerHTML = discountPrice + "€";
