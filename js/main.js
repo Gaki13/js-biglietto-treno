3d7df57a635b94faf23c6454283a10e44c83486c
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-04-28 16:18:39 +0200

    aggiunta condizione per il calcolo prezzo del biglietto

--- js/main.js
+++ js/main.js
@@ -6,7 +6,18 @@ const age = Number(prompt("Quanti anni ha il passeggero?"))
 let price = Kilometers * 0.21
 console.log(price);
 //4.SE il passeggero è minorenne applico uno sconto del 20%
-
 //5.SE il passeggerto è over 65 anno applico uno sconto del 60%
+//6.SE l'età del passeggero è compresa tra 18 e 65 non applico nessuno sconto
+if (age < 18 ) {
+    let discountChilds = price * 0.2
+    let discountPrice = price - discountChilds;
+    document.getElementById("final-price").innerHTML = discountPrice;
+} else if ( age >= 65 ){
+    let discountOlds = price * 0.6
+    let discountPrice = price - discountOlds
+    document.getElementById("final-price").innerHTML = discountPrice;
+} else {
+    document.getElementById("final-price").innerHTML = price;
+}
 
-//6.Assicurarsi che il prezzo finale venga mostrato in massimo 2 decimali
+//7.Assicurarsi che il prezzo finale venga mostrato in massimo 2 decimali
